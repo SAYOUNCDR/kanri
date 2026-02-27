aa506f5d4102d625c1d745b908d7e5643dad4c7a
0xSYN.dev <pheonixgamer12345@gmail.com>
2026-02-27 22:10:36 +0530

    feat: add register and login controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model");
+const { generateAccessToken, generateRefreshToken } = require("../lib/token");
+const bcrypt = require("bcrypt");
 
 
 const registerUser = async (req, res) => {
@@ -8,9 +10,36 @@ const registerUser = async (req, res) => {
         if (user) {
             return res.status(400).json({ message: "User already exists" });
         }
-        const newUser = await User.create({ username, email, password });
-        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({ username, email, password: hashedPassword });
+
         res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const accessToken = generateAccessToken(user._id, user.role, user.tokenVersion);
+        const refreshToken = generateRefreshToken(user._id, user.role);
+    
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        });
+        res.json({ accessToken });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
